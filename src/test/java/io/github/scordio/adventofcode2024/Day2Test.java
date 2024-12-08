1a9166b1bbd8516ca4b6513451c078b257066a1c
Stefano Cordio <stefano.cordio@gmail.com>
2024-12-08 14:46:36 +0100

    Add day 2, part 2, solution (2024)

--- src/test/java/io/github/scordio/adventofcode2024/Day2Test.java
+++ src/test/java/io/github/scordio/adventofcode2024/Day2Test.java
@@ -1,7 +1,6 @@
 package io.github.scordio.adventofcode2024;
 
 import io.github.scordio.ScanInput;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -10,12 +9,14 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Gatherer;
+import java.util.stream.Stream;
 
 import static java.lang.Boolean.TRUE;
 import static java.lang.Integer.signum;
 import static java.lang.Math.abs;
 import static java.util.Arrays.stream;
 import static java.util.stream.Gatherers.windowSliding;
+import static java.util.stream.IntStream.range;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Day 2: Red-Nosed Reports")
@@ -35,14 +36,16 @@ class Day2Test {
     assertEquals(expected, answer);
   }
 
-  @Disabled("not implemented")
   @ParameterizedTest
   @CsvSource({
     "day2-example1, 4",
-    "day2, -1",
+    "day2, 308",
   })
   void part2(@ScanInput Scanner scanner, int expected) {
-    long answer = 0;
+    long answer = scanner.tokens()
+      .map(Report::new)
+      .filter(Report::isSafeWithProblemDumper)
+      .count();
 
     assertEquals(expected, answer);
   }
@@ -58,6 +61,21 @@ class Day2Test {
     }
 
     boolean isSafe() {
+      return isSafe(levels);
+    }
+
+    boolean isSafeWithProblemDumper() {
+      return isSafe() || range(0, levels.size())
+        .boxed()
+        .map(i -> listWithoutIndex(levels, i))
+        .anyMatch(Report::isSafe);
+    }
+
+    private <T> List<T> listWithoutIndex(List<T> list, int i) {
+      return Stream.of(list.subList(0, i), list.subList(i + 1, list.size())).flatMap(List::stream).toList();
+    }
+
+    private static boolean isSafe(List<Integer> levels) {
       return levels.stream()
         .gather(windowSliding(2))
         .gather(matchingRules())
